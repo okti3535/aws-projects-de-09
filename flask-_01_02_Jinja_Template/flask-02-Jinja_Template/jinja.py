ca9e615a609de1404514f8d96d15204a2d492034
oktay <oktayebru89@gmail.com>
2024-06-28 11:49:12 +0300

    app.run changed

--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -11,5 +11,5 @@ def number():
      return render_template('body.html', value1=x, value2=y, sum=x*y)
 
 if __name__== "__main__":
-     app.run(debug=True)
-     # app.run(host= '0.0.0.0', port=80)
+     # app.run(debug=True)
+     app.run(host= '0.0.0.0', port=80)
